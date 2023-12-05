7df3879b35941dbf9109d3f5308bc11c74ec5b75
jdi0901 <jdi0901@naver.com>
2023-12-06 00:31:39 +0900

    jpashop commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        //Application 로딩 시점에 딱 하나만 존재해야함.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션의 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Order order = em.find(Order.class, 1L);
+            order.getMember();
+
+
+            //쿼리 실행 시점
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
